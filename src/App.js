d876bacd3593f3ef097d579c1e452e1fa1bd4e03
Alexey Panteleychuk <alexeypanteleychuk@MacBook-Pro-Alexey.local>
2018-08-20 19:06:26 +0300

    some corrections

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
       message: '',
     });
   }
+  
   handleSubmit(e) {
     e.preventDefault();
     const isOnTheList = this.state.buyItems.includes(this.state.input)
